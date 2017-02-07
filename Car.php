6fc429fe265346e16052ffa191db4855fff14e26
Oliver Reipschlaeger <thepsi43@gmail.com>
2017-02-07 10:44:41 +0100

    Inheritance example with the child of a class trying to override a parent method that is final, which results in an error.

--- Car.php
+++ Car.php
@@ -1,19 +1,21 @@
 <?php
 // The parent class has hello method that returns "beep".
+
 class Car {
-    public function hello()
+    final public function hello()
     {
         return "beep";
     }
 }
 
-//The child class has hello method that returns "Halllo"
+//The child class has hello method that tries to override the hello method in the parent
 class SportsCar extends Car {
     public function hello()
     {
         return "Hallo";
     }
 }
+
 
 //Create a new object
 $sportsCar1 = new SportsCar();
